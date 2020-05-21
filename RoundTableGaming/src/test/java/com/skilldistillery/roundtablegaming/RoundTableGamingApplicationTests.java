bf4f1c4659594e50981988b3665db5d19039c8c5
Jacob Zieleniewski <jacobzieleniewski@gmail.com>
2020-05-21 11:28:54 -0600

    Spring Boot setup

---
+++ RoundTableGaming/src/test/java/com/skilldistillery/roundtablegaming/RoundTableGamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.roundtablegaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoundTableGamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
