ee9544f676462b93db398e2f74201a927792360f
Sergei Samoiloff <sergio.samoiloff@gmail.com>
2020-05-21 13:23:40 -0600

    Build User entity and test case, configure persistence.xml

--- JPAMidterm/src/main/java/com/skilldistillery/roundtablegaming/entities/User.java
+++ JPAMidterm/src/main/java/com/skilldistillery/roundtablegaming/entities/User.java
@@ -1,5 +1,100 @@
 package com.skilldistillery.roundtablegaming.entities;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String username;
+	private String password;
+	private boolean enabled;
+	private String role;
+
+	public User() {
+		super();
+	}
+
+	public User(int id, String username, String password, boolean enabled, String role) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.enabled = enabled;
+		this.role = role;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("User [id=").append(id).append(", username=").append(username).append(", password=")
+				.append(password).append(", enabled=").append(enabled).append(", role=").append(role).append("]");
+		return builder.toString();
+	}
 }
--- JPAMidterm/src/test/java/com/skilldistillery/roundtablegaming/entities/UserTest.java
+++ JPAMidterm/src/test/java/com/skilldistillery/roundtablegaming/entities/UserTest.java
@@ -2,33 +2,51 @@ package com.skilldistillery.roundtablegaming.entities;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class UserTest {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private User user;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("RoundTable");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf.close();
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		user = em.find(User.class, 1);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		user = null;
+		em.close();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("testing user mapping")
+	void test1() {
+		assertNotNull(user);
+		assertEquals("admin", user.getUsername());
+		assertEquals("admin", user.getPassword());
+		assertTrue(user.isEnabled());
 	}
 
 }
