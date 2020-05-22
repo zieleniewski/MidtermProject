64290f81ad50a27c4f9ecfd43896604d87c78893
Shane Duncan <aequitas321@icloud.com>
2020-05-22 17:38:11 -0400

    pushing beggining of dao

--- JPAMidterm/src/main/java/com/skilldistillery/roundtablegaming/data/AddressDAO.java
+++ JPAMidterm/src/main/java/com/skilldistillery/roundtablegaming/data/AddressDAO.java
@@ -1,5 +1,12 @@
 package com.skilldistillery.roundtablegaming.data;
 
-public class AddressDAO {
+import com.skilldistillery.roundtablegaming.entities.Address;
 
+public interface AddressDAO {
+
+	public Address createAddress(Address address);
+	
+	public Address deleteAddress(int id);
+	
+	public Address updateAddress(int id, Address updatedAddress);
 }
