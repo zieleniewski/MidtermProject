4d0b35f8a78a663d170f0367126ed603373a2d05
mikematosky <mikematosky@hotmail.com>
2020-05-23 22:31:01 -0400

    Working EventComments and refactored GameDAOs

---
+++ RoundTableGaming/src/main/java/com/skilldistillery/roundtablegaming/data/EventCommentDAO.java
@@ -0,0 +1,22 @@
+package com.skilldistillery.roundtablegaming.data;
+
+import java.util.List;
+
+import com.skilldistillery.roundtablegaming.entities.EventComment;
+
+public interface EventCommentDAO {
+	
+	public EventComment createEventComment(EventComment ev);
+	
+	public EventComment updateEventComment(EventComment ev);
+	
+	public List<EventComment> getAllEventComments();
+	
+	public List<EventComment> getEventCommentsByEventId(int eventId);
+	
+	//don't know if we need this. Maybe in a for loop?
+	public EventComment getEventCommentById(int id);
+	
+	public boolean deleteEventComment(int id);
+	
+}
