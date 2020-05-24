97b5f73f0256afbf13a11bad270ee5271205da1e
mikematosky <mikematosky@hotmail.com>
2020-05-24 17:56:24 -0400

    Category work

--- RoundTableGaming/src/main/java/com/skilldistillery/roundtablegaming/data/CategoryDAOImpl.java
+++ RoundTableGaming/src/main/java/com/skilldistillery/roundtablegaming/data/CategoryDAOImpl.java
@@ -1,5 +1,48 @@
 package com.skilldistillery.roundtablegaming.data;
 
-public class CategoryDAOImpl {
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
+import org.springframework.stereotype.Service;
+
+import com.skilldistillery.roundtablegaming.entities.Category;
+
+@Service
+@Transactional
+public class CategoryDAOImpl implements CategoryDAO{
+
+	@PersistenceContext
+	EntityManager em;
+
+	@Override
+	public Category updateCategory(Category category) {
+		Category updated= null;
+		updated= em.find(Category.class, category.getId());
+		
+		updated.setDescription(category.getDescription());
+		updated.setGames(category.getGames());
+		updated.setName(category.getName());
+		
+		em.persist(updated);
+		em.flush();
+		
+		return updated;
+	}
+
+	@Override
+	public Category createCategory(Category newCategory) {
+		em.persist(newCategory);
+		em.flush();
+		
+		return newCategory;
+	}
+
+	@Override
+	public boolean disableCategory(int categoryId) {
+		
+		return false;
+	}
+	
+	
 }
