bbc1a2d3a4aa3525ad75011882e153972c866b21
Sergei Samoiloff <sergio.samoiloff@gmail.com>
2020-05-24 15:24:44 -0600

    Minor edits to Address/Attendee entities; Completed AttendeeDAO/Impl; Updated schema diagram

--- RoundTableGaming/src/main/java/com/skilldistillery/roundtablegaming/data/AttendeeDAO.java
+++ RoundTableGaming/src/main/java/com/skilldistillery/roundtablegaming/data/AttendeeDAO.java
@@ -1,10 +1,12 @@
 package com.skilldistillery.roundtablegaming.data;
 
 import com.skilldistillery.roundtablegaming.entities.Attendee;
+import com.skilldistillery.roundtablegaming.entities.AttendeeId;
 
 public interface AttendeeDAO {
 
 	public Attendee create(Attendee attn);
-	public Attendee update(Attendee attn);
-	public Attendee delete(Attendee attn);
+	public Attendee update(Attendee attn, AttendeeId id);
+	public Attendee enable(AttendeeId id);
+	public Attendee disable(AttendeeId id);
 }
--- RoundTableGaming/src/main/java/com/skilldistillery/roundtablegaming/data/AttendeeDAOImpl.java
+++ RoundTableGaming/src/main/java/com/skilldistillery/roundtablegaming/data/AttendeeDAOImpl.java
@@ -1,25 +1,56 @@
 package com.skilldistillery.roundtablegaming.data;
 
-import com.skilldistillery.roundtablegaming.entities.Attendee;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
+import org.springframework.stereotype.Service;
+
+import com.skilldistillery.roundtablegaming.entities.Attendee;
+import com.skilldistillery.roundtablegaming.entities.AttendeeId;
+
+@Service
+@Transactional
 public class AttendeeDAOImpl implements AttendeeDAO {
 
+	@PersistenceContext
+	private EntityManager em;
+	
 	@Override
 	public Attendee create(Attendee attn) {
-		// TODO Auto-generated method stub
-		return null;
+		em.persist(attn);
+		em.flush();
+		return attn;
 	}
 
 	@Override
-	public Attendee update(Attendee attn) {
-		// TODO Auto-generated method stub
-		return null;
+	public Attendee update(Attendee attn, AttendeeId id) {
+		Attendee updated = em.find(Attendee.class, id);
+		updated.setUser(attn.getUser());
+		updated.setEventGame(attn.getEventGame());
+		updated.setEventRating(attn.getEventRating());
+		updated.setAttendeeComment(attn.getAttendeeComment());
+		em.persist(updated);
+		em.flush();
+		return updated;
 	}
 
 	@Override
-	public Attendee delete(Attendee attn) {
-		// TODO Auto-generated method stub
-		return null;
+	public Attendee enable(AttendeeId id) {
+		Attendee enabled = em.find(Attendee.class, id);
+		enabled.setEnabled(true);
+		em.persist(enabled);
+		em.flush();
+		return enabled;
+	}
+
+	@Override
+	public Attendee disable(AttendeeId id) {
+		Attendee disabled = em.find(Attendee.class, id);
+		disabled.setEnabled(false);
+		em.persist(disabled);
+		em.flush();
+		return disabled;
 	}
 
 }
